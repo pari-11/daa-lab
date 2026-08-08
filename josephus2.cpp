7a7264928189a64e81793b7c09e175698fb249c1
pari-11 <parnikaa.thakur.2711@gmail.com>
2026-08-08 12:50:55 +0530

    Update josephus2.cpp

--- josephus2.cpp
+++ josephus2.cpp
@@ -1,52 +1,62 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-struct Node {
+class Node {
+    public:
     int data;
     Node* next;
 
-    Node(int x) {
-        data=x;
-        next=NULL;
+    Node(int value) {
+        data = value;
+        next = NULL;
     }
 };
 
-int josephusLinkedList(int n, int k) {
-    Node* head=new Node(1);
-    Node* temp=head;
-    for (int i=2; i<=n; i++) {
-        temp->next=new Node(i);
-        temp=temp->next;
-    }
-
-    temp->next=head;
-
-    Node* prev=temp;
-    Node* curr=head;
-
-    while (curr!=curr->next) {
-
-        for (int i=1; i<k; i++) {
-            prev=curr;
-            curr=curr->next;
-        }
-
-        prev->next=curr->next;
-
-        Node* del=curr;
-        curr=curr->next;
-
-        delete del;
-    }
-
-    int ans=curr->data;
-    delete curr;
-
-    return ans;
-}
-
 int main() {
     int n, k;
-    cin>>n>>k;
-    cout<<"Survivor = "<<josephusLinkedList(n,k);
+    cout<<"Enter the number of people: ";
+    cin>>n;
+
+    cout<<"Enter the counting number: ";
+    cin>>k;
+
+    //create a linked list
+    Node* head = new Node(1);
+    Node* temp = head;
+
+    for(int i=2; i<=n; i++) {
+        temp->next = new Node(i);
+        temp = temp->next;
+    }
+
+    //make it circular
+    temp->next = head;
+
+    //current previous
+    Node* current = head;
+    Node* previous = temp;
+
+    //elimination
+    while(current->next != current) {//until there is one node left this loop will run 
+        
+        //moving k positions
+        for(int i=1; i<k; i++) {
+            previous = current;
+            current = current->next;
+        }
+
+        cout<<"Eliminated: "<< current->data<<endl;
+
+        //deleting current
+        previous->next = current->next;
+        delete current;
+
+        current = previous->next;
+    } 
+
+    cout<<"Survivor: "<< current->data<<endl;
+
+    delete current;
+    return 0;
+    
 }
