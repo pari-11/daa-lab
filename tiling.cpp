a5659be0f38e620b93c5bc36662e105c52335a11
pari-11 <parnikaa.thakur.2711@gmail.com>
2026-09-03 16:25:50 +0530

    tiling
    Updated the tiling program to include comments and correct base cases.

--- tiling.cpp
+++ tiling.cpp
@@ -2,19 +2,34 @@
 using namespace std;
 
 int main() {
+    // Variable to store the number of columns
     int n;
-    cout<<"enter number of cols: ";
+
+    // Take the number of columns as input
+    cout << "Enter number of cols: ";
     cin >> n;
 
+    // Array to store the number of ways for each number of columns
     int ways[n + 1];
 
-    ways[0] = 1;
+    // Base cases
+    // There are 0 ways for 0 columns
+    ways[0] = 0;
+
+    // There is 1 way for 1 column
     ways[1] = 1;
 
-    for (int i = 2; i <= n; i++) {
+    // There are 2 ways for 2 columns
+    ways[2] = 2;
+
+    // Calculate the number of ways for 3 to n columns
+    for (int i = 3; i <= n; i++) {
+
+        // Current value = sum of the previous two values
         ways[i] = ways[i - 1] + ways[i - 2];
     }
 
+    // Display the total number of ways
     cout << ways[n] << endl;
 
     return 0;
