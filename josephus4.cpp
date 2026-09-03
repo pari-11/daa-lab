9378d80e397502a736541576de39744b651447f8
pari-11 <parnikaa.thakur.2711@gmail.com>
2026-09-03 16:18:20 +0530

    josephus 4

--- josephus4.cpp
+++ josephus4.cpp
@@ -1,15 +1,44 @@
 #include <iostream>
 using namespace std;
 
+
+// Function to find the Josephus survivor using binary representation
+// This method works for the Josephus problem when k = 2
 int josephusBinary(int n) {
-    int power=1;
-    while (power*2<=n)
-        power*=2;
-    return 2*(n-power)+1;
+
+    // Start with the smallest power of 2
+    int highestPower = 1;
+
+    // Find the largest power of 2 that is less than or equal to n
+    while (highestPower * 2 <= n) {
+        highestPower = highestPower * 2;
+    }
+
+    // Find the number of people remaining after removing
+    // the largest power of 2 from n
+    int remaining = n - highestPower;
+
+    // Calculate the survivor
+    // Formula for k = 2:
+    // Survivor = 2 * remaining + 1
+    int survivor = 2 * remaining + 1;
+
+    // Return the survivor
+    return survivor;
 }
 
+
 int main() {
+
+    // Variable to store the number of people
     int n;
-    cin>>n;
-    cout<<"Survivor = "<<josephusBinary(n);
+
+    // Take the number of people as input
+    cout << "Enter number of people: ";
+    cin >> n;
+
+    // Call the function and display the survivor
+    cout << "Survivor = " << josephusBinary(n);
+
+    return 0;
 }
